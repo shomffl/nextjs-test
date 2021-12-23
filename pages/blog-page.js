066d2bec3ab755dd6327c50f0f698318f368d1b6
shomffl <showatanabe0226@gmail.com>
2021-12-23 20:43:49 +0900

    initial commit

---
+++ pages/blog-page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from "../components/Layout"
+
+const Blog = () => {
+  return (
+    <Layout title="Blog">
+      Blog page
+    </Layout>
+  )
+}
+
+export default Blog
