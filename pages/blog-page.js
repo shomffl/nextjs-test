70f71e0cb48ba7d858c8915ccf662d44df121c31
shomffl <showatanabe0226@gmail.com>
2021-12-24 21:07:25 +0900

    getStaticPropsの実装

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import { Post } from "../components/Post";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <Layout title="Blog">
-      Blog page
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
     </Layout>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
